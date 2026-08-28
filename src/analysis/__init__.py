f118114400dd34376dfd355969da7009f3cd8c94
MarMars1 <162321532+MarMars1@users.noreply.github.com>
2026-08-28 15:26:52 +0200

    Add files via upload

---
+++ src/analysis/__init__.py
@@ -0,0 +1,17 @@
+from .field_features import (
+    compute_spatial_gradients,
+    compute_divergence,
+    compute_curl,
+    compute_angle,
+    compute_eigenvalues,
+    compute_field_features,
+)
+
+__all__ = [
+    "compute_spatial_gradients",
+    "compute_divergence",
+    "compute_curl",
+    "compute_angle",
+    "compute_eigenvalues",
+    "compute_field_features",
+]
