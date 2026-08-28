d5811d07ac9feb5e53d7e4c6e8be1490785d76f3
MarMars1 <162321532+MarMars1@users.noreply.github.com>
2026-08-28 15:48:49 +0200

    Update experiment.py

--- src/utils/experiment.py
+++ src/utils/experiment.py
@@ -31,3 +31,30 @@ def create_experiment_directory(
     )
 
     return experiment_dir
+
+
+def save_experiment_summary(
+    output_dir: Path,
+    names,
+):
+    """
+    Save summary of experiment.
+    """
+
+    with open(
+        output_dir /
+        "experiment_summary.txt",
+        "w",
+        encoding="utf-8",
+    ) as f:
+
+        f.write(
+            f"Number of systems: {len(names)}\n"
+        )
+
+        f.write("\nSystems:\n")
+
+        for name in names:
+            f.write(
+                f" - {name}\n"
+            )
